824e8127ec2808adeeffe3dc2bebff3faf3c061d
Ivanna Bura <56138602+ibura001@users.noreply.github.com>
2021-04-02 23:10:04 +0300

    Add files via upload

---
+++ app-frontend/src/test/java/com/kpi/microservices/appfrontend/AppFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.microservices.appfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
